a90ad87e4f71342e420488474a90d39daabef6ab
Aixx <aixxturlan@gmail.com>
2023-12-28 14:07:11 -0300

    Animación damage y gravedad bug fix en la muerte de la rana

--- Frog.gd
+++ Frog.gd
@@ -10,7 +10,6 @@ var player: Node
 var chase: bool = false 
 var direction: Vector2 #Hacia donde se encuentra el player
 
-@onready var timerChase: Timer = $Chase
 @onready var anim: Node = get_node("Frog-anim")
 
 func _on_player_detection_body_entered(body):
@@ -20,7 +19,7 @@ func _on_player_detection_body_entered(body):
 func _on_player_detection_body_exited(body):
 	if body.name == "Player":
 		chase = false
-		timerChase.stop() #Si no persigue, desactiva el timer del salto
+		$Chase.stop() #Si no persigue, desactiva el timer del salto
 
 func _physics_process(delta):
 	#Gravedad
@@ -30,9 +29,9 @@ func _physics_process(delta):
 	#El normalized transforma el vector original en vector unitario
 	direction = (player.position - self.position).normalized() 
 	if chase == true:
-			if is_on_floor() && timerChase.is_stopped():
-				_on_timer_timeout() #Como el timer esta apagado por default arranca saltando
-				timerChase.start() #Inicio el timer que regula el salto
+			if is_on_floor() && $Chase.is_stopped():
+				jump() #Como el timer esta apagado por default arranca saltando
+				$Chase.start() #Inicio el timer que regula el salto
 			elif not is_on_floor() && velocity.y > 0:
 				get_node("Frog-anim").play("fall")
 			elif not is_on_floor():
@@ -64,9 +63,32 @@ func jump():
 func _on_top_checker_body_entered(body):
 	if body.name == "Player":
 		chase = false
-		timerChase.stop()
+		$Chase.stop()
 		body.bounce()
+		gravity = 0
+		
 		get_node("Frog-anim").play("death")
+		set_collision_layer_value(5,false)
+		set_collision_mask_value(1,false)
+		$side_checker.set_collision_mask_value(1,false)
+		$"Player-Detection".set_collision_mask_value(1,false)
 		$top_checker.set_collision_mask_value(1,false) #Elimina la collision mask y evita que el pj rebote de mas 
 		await get_node("Frog-anim").animation_finished
 		self.queue_free()
+
+func _on_side_checker_body_entered(body):
+	if body.name == "Player":
+		body.ouch(direction.x)
+		$damage_player.start()
+		set_collision_layer_value(5,false)
+		set_collision_mask_value(1,false)
+		$top_checker.set_collision_layer_value(5,false)
+		$top_checker.set_collision_mask_value(1,false)
+
+func _on_damage_player_timeout():
+	set_collision_layer_value(5,true)
+	set_collision_mask_value(1,true)
+	if is_on_floor():
+		$top_checker.set_collision_layer_value(5,true)
+		$top_checker.set_collision_mask_value(1,true)
+		$damage_player.stop()
--- Player/Player.gd
+++ Player/Player.gd
@@ -6,42 +6,70 @@ const JUMP_VELOCITY = -350.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var hurt: bool = false
+var hurt_control: int = 0
 @onready var anim = get_node("AnimationPlayer")
-
-func bounce():
-	velocity.y = JUMP_VELOCITY * 0.7
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	
+	if not hurt:
+		# Handle jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			anim.play("jump")
+		
+		# Para que la altura del salto dependa de cuanto se presiona la barra
+		if Input.is_action_just_released("ui_accept") && velocity.y < 0:
+			velocity.y = 0
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		anim.play("jump")
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction = Input.get_axis("ui_left", "ui_right")
+		
+		if direction == -1:
+			get_node("AnimatedSprite2D").flip_h = true
+		elif direction == 1: 
+			get_node("AnimatedSprite2D").flip_h = false
 	
-	# Para que la altura del salto dependa de cuanto se presiona la barra
-	if Input.is_action_just_released("ui_accept") && velocity.y < 0:
-		velocity.y = 0
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * SPEED
+			if velocity.y == 0 && is_on_floor():
+				anim.play("run")
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			if velocity.y == 0:
+				anim.play("idle")
+			elif velocity.y > 0:
+				anim.play("fall")
 	
-	if direction == -1:
-		get_node("AnimatedSprite2D").flip_h = true
-	elif direction == 1: 
-		get_node("AnimatedSprite2D").flip_h = false	
-	
-	if direction:
-		velocity.x = direction * SPEED
-		if velocity.y == 0 && is_on_floor():
-			anim.play("run")
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if velocity.y == 0:
-			anim.play("idle")
-		elif velocity.y > 0:
-			anim.play("fall")
 	move_and_slide()
+
+func bounce():
+	velocity.y = JUMP_VELOCITY * 0.7
+
+func ouch(enemy_direction: float):
+	hurt = true
+	$hurt_timer.start()
+	anim.play("hurt")
+	set_modulate(Color(1,0.3,0.3,0.7))
+	velocity.y = JUMP_VELOCITY * 0.6
+	
+	if enemy_direction > 0:
+		velocity.x = 100
+	elif enemy_direction < 0:
+		velocity.x = -100
+	
+	Input.action_release("ui_left") 
+	Input.action_release("ui_right")
+	Input.action_release("ui_up")
+
+func _on_hurt_timer_timeout():
+	set_modulate(Color(1,1,1,1))
+	hurt = false
+	if is_on_floor():
+		$hurt_timer.stop()
+		velocity.x = 0
+
