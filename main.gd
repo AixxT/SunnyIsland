87dcd1a0d331d569f844ebc1dd6f3bbc9bd4df6d
Aixx <aixxturlan@gmail.com>
2024-01-16 16:52:12 -0300

    Changed the main menu

--- main.gd
+++ main.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 
 
@@ -6,6 +6,6 @@ func _on_quit_pressed():
 	get_tree().quit()
 	
 
-
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://world.tscn")
+
